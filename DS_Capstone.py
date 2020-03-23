063df75b75a08d82b37623ce2a76359d22a22c23
T-65XJ <jolsensw@gmail.com>
2020-03-22 18:21:00 -0700

    Initial commit
    :wq

---
+++ DS_Capstone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Applied Data Science Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
